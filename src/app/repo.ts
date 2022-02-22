a415979d59aaf5234fcf430c899c8cbd0ee8d4b7
silvianjoki <10silvianjoki@gmail.com>
2022-02-22 12:33:16 +0300

    add repo constructor

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,10 @@
 export class Repo {
+    constructor( 
+        public name:string,
+        public html_url:string,
+        public forks:any,
+        public id:number, 
+        public description:string,  
+        public language:string,
+        public updated_at:Date ) {}
 }
