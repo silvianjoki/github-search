dc7e8dc8bd8405166f4880f24bd534e197ac7abf
silvianjoki <10silvianjoki@gmail.com>
2022-02-22 12:34:14 +0300

    edit user constructor

--- src/app/users.ts
+++ src/app/users.ts
@@ -2,12 +2,12 @@ export class Users {
     constructor(
     public username:string,
     public login:string,
-    public id:number,
-    public followers: number,
+    public id:any,
+    public followers: any,
     public bio:string,
     public following: number,
     public public_repos: number,
-    public avatar_url:string,
+    public avatar_url:any,
     public created_at:string,) {}
 
 }
