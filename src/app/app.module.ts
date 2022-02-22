27fcc49117f382d7824709acd5d66d8d63f150f1
Silvia <silvianjoki@Silvias-MacBook-Air.local>
2022-02-22 12:22:32 +0300

    import mat tabs module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { DatecountPipe } from './datecount.pipe';
 import { VistedrepoDirective } from './vistedrepo.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { VistedrepoDirective } from './vistedrepo.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule, 
+    RouterModule, 
+    FormsModule, 
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
