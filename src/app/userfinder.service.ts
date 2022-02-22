1f921fea4a5082f5e3ba5ca0d319d48db2647184
silvianjoki <10silvianjoki@gmail.com>
2022-02-22 12:40:26 +0300

    add service edit

--- src/app/userfinder.service.ts
+++ src/app/userfinder.service.ts
@@ -7,9 +7,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class UserfinderService {
 
   private username: string;
-  private repo: string;
+  private repo!: string;
   private clientid = '2823a44a04bb254e'
   private clientsecret ='3e35f41328d7f5c67900c8cbfaf5bd94c471f495'
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.username = 'silvianjoki'
+  }
+
+  findUser() {
+    interface ApiResponse {
+      login:string
+    }
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+  } 
+  findRepo() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+  }
+  fetchRepo() {
+    return this.http.get('https://api.github.com/search/repositories?q=' + this.repo + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+  }
 }
